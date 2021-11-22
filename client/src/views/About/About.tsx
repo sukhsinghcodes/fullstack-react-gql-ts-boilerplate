2b90df8f82c3dd86eab6c8e5950564c7cb5f3169
Sukhvinder Singh <sukhvinder@durhailay.co>
2021-11-22 13:26:20 +0000

    Updated about page

--- client/src/views/About/About.tsx
+++ client/src/views/About/About.tsx
@@ -2,5 +2,10 @@ import React from 'react'
 import { PageTitle } from '../../components/Titles'
 
 export const About = () => {
-  return <PageTitle>About</PageTitle>
+  return (
+    <>
+      <PageTitle>About</PageTitle>
+      <p>This is an about page</p>
+    </>
+  )
 }
